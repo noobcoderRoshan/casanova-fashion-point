6f4045c6eac2569d92dbb0e4d8ac2df43352734a
noobcoderRoshan <RK9055436@gmail.com>
2026-05-24 12:23:29 +0530

    Fix: Run migrations on app startup

--- mens_fashion/core/wsgi.py
+++ mens_fashion/core/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Initialize Django
+django.setup()
+
+# Run migrations on startup
+try:
+    call_command('migrate', '--noinput', verbosity=0)
+except Exception as e:
+    print(f"Migration warning: {e}")
 
 application = get_wsgi_application()
